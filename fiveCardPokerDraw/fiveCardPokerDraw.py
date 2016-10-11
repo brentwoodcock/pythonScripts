3a88bbcb5aaeca1d33c4405f5f03d0a160955698
Brent Woodcock <brentwoodcock@gmail.com>
2016-10-11 05:08:23 -0500

    added initial poker game (still need to do Poker class)

---
+++ fiveCardPokerDraw/fiveCardPokerDraw.py
@@ -0,0 +1,97 @@
+import random
+
+class Card (object):
+  RANKS = (2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14)
+
+  SUITS = ('C', 'D', 'H', 'S')
+
+  def __init__ (self, rank = 12, suit = 'S'):
+    if (rank in Card.RANKS):
+      self.rank = rank
+    else:
+      self.rank = 12
+
+    if (suit in Card.SUITS):
+      self.suit = suit
+    else:
+      self.suit = 'S'
+
+  def __str__ (self):
+    if (self.rank == 14):
+      rank = 'A'
+    elif (self.rank == 13):
+      rank = 'K'
+    elif (self.rank == 12):
+      rank = 'Q'
+    elif (self.rank == 11):
+      rank = 'J'
+    else:
+      rank = str (self.rank)
+    return rank + self.suit
+
+  def __eq__ (self, other):
+    return (self.rank == other.rank)
+
+  def __ne__ (self, other):
+    return (self.rank != other.rank)
+
+  def __lt__ (slef, other):
+    return (self.rank < other.rank)
+
+  def __le__ (self, other):
+    return (self.rank <= other.rank)
+
+  def __gt__ (self, other):
+    return (self.rank > other.rank)
+
+  def __ge__ (self, other):
+    return (self.rank >= other.rank)
+
+class Deck (object):
+  def __init__ (self):
+    self.deck = []
+    for suit in Card.SUITS:
+      for rank in Card.RANKS:
+        card = Card (rank, suit)
+        self.deck.append (card)
+
+  def shuffle (self):
+    random.shuffle (self.deck)
+
+  def deal (self):
+    if (len(self.deck) == 0):
+      return None
+    else:
+      return self.deck.pop(0)
+
+class Poker (object):
+  def __init__ (self, num_players):
+    self.deck = Deck()
+    self.deck.shuffle()
+    self.players = []
+    numcards_in_hand = 5
+
+    for i in range (num_players):
+      hand = []
+      for j in range (numcards_in_hand):
+        hand.append (self.deck.deal())
+      self.players.append (hand)
+
+
+
+
+
+
+def main():
+  #  Prompt user to enter the nuimber of players
+  num_players = int (input ('Enter number of players: '))
+  while ((num_players < 2) or (num_players > 6)):
+    num_players = int (input ('Enter number of players(must be less than 6 or greater than 2): '))
+
+  #  Create the poker object
+  game = Poker (num_players)
+
+  #  Play the game
+  game.play()
+
+main()
